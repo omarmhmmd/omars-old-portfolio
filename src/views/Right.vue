f6102e292349cb86897555d534311ac5062fcf7a
omarmhmmd <omar.mhmmd@gmail.com>
2019-11-07 16:52:37 -0800

    get dist folder working w/ netlify

--- src/views/Right.vue
+++ src/views/Right.vue
@@ -1,7 +1,7 @@
 <template>
 <div @scroll="onScroll" class="right-container">
-  <div v-waypoint="{ active: true, callback: onWaypoint, options: intersectionOptions }" :id="test.tag" class="images" v-for="test in newJson" :key="test">
-    <img class = "lazy-img-fadeIn" v-for="(item, index) in test.images" :id="test.tag" :key="item" v-lazy="test.images[index]" lazy="loaded">
+  <div class="images" v-for="test in newJson" :key="test">
+    <img  v-waypoint="{ active: true, callback: onWaypoint, options: intersectionOptions }"  class = "lazy-img-fadeIn" v-for="(item, index) in test.images" :id="test.tag" :key="item" v-lazy="test.images[index]" lazy="loaded">
     <!-- <img data-aos="fade" v-for="(item, index) in test.images" :id="test.tag" :key="item" v-lazy="test.images[index]" lazy="loaded"> -->
   </div>
 </div>
@@ -20,7 +20,7 @@ export default {
       activeProjectTag: '',
       projectJson,
       intersectionOptions: {
-        threshold: [0.125]
+        threshold: [0.5]
       }
     }
   },
